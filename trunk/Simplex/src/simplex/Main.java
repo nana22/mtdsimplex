36dd1acdcb1c6b80488ea6b189b939694231b0f7
marc.antoni.28 <marc.antoni.28@32f8e480-a95e-11de-a118-c7387b1e4f66>
2009-09-30 21:32:14 +0000

    Cambiado Hola por Perro

--- trunk/Simplex/src/simplex/Main.java
+++ trunk/Simplex/src/simplex/Main.java
@@ -1,23 +1,3 @@
-/*
- * @(#)Main.java
- * @charset "utf-8";
- *
- * Copyright (C) 2009  Marco Maldonado, Freddy Barrera.
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
 package simplex;
 
 import simplex.ui.Principal;
@@ -34,7 +14,7 @@ public class Main {
      */
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("hOLA");
+            System.out.println("Perro");
         } else {
             System.out.println(args);
         }
