1159a028dbd7a52fac6dcb38408c36ebd0b93df8
marc.antoni.28 <marc.antoni.28@32f8e480-a95e-11de-a118-c7387b1e4f66>
2009-10-08 09:32:49 +0000

    Modificacion en Conversiones para obtener el numero de variables Llenando columnas de SimplexTable

--- Simplex/src/simplex/resolvedor/mate/SimplexTable.java
+++ Simplex/src/simplex/resolvedor/mate/SimplexTable.java
@@ -8,12 +8,15 @@ public class SimplexTable {
 
 private String colNames[];
 private String rowData[][];
-
+private String varBasic[];
+private String var[];
+    //FOc = Funcion Objetivo Convertida, restC = Restricciones Convertidas;
     SimplexTable( String var[], String varBasic[], Ecuacion FOc, Ecuacion restC[] ){
     }
     public void setColNames( String var[], String varBasic[] ){
         int tamVar = var.length;
         int tamVarBasic = varBasic.length;
         int vars = tamVar + tamVarBasic;
+        int numCol = vars + 4;
     }
 }
