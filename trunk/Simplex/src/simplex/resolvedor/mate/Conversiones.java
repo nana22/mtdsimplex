3b96b2da80000fcba8a0578ec934a24c9684680b
marc.antoni.28 <marc.antoni.28@32f8e480-a95e-11de-a118-c7387b1e4f66>
2009-10-08 09:32:49 +0000

    Modificacion en Conversiones para obtener el numero de variables Llenando columnas de SimplexTable

--- trunk/Simplex/src/simplex/resolvedor/mate/Conversiones.java
+++ trunk/Simplex/src/simplex/resolvedor/mate/Conversiones.java
@@ -7,11 +7,20 @@ package simplex.resolvedor.mate;
 
 public class Conversiones {
 
+private String varBasic[];
+private String tempVB[];
+private String var[];
+
     public Conversiones( Ecuacion FO, Ecuacion [] rest ){
         tratarFO(FO);
         tratarRestric(rest);
     }
     public void tratarFO( Ecuacion FO ){
+        Monomio monomiosFO[] = FO.getMonomios();
+        var = new String[monomiosFO.length];
+        for (int i=0; i<monomiosFO.length; i++){
+            var[i] = Character.toString(monomiosFO[i].getVariable());
+        }
         Monomio z = FO.getMonomioResultado();
         z.setCoeciente(-1);
         FO.setMonomioResultado(null);
@@ -24,7 +33,7 @@ public class Conversiones {
         for (int i=0; i<tama; i++){
             //Checando restricciones
             int igualdad = restricciones[i].getTipoIgualdad();
-            char holgura = 64;
+            char holgura = 99;
             switch (igualdad){
                 case 0: 
                     //Se Conserva igual
@@ -32,15 +41,33 @@ public class Conversiones {
                 case 3:
                     restricciones[i].setTipoIgualdad(0);
                     restricciones[i].addMononio( new Monomio( -1 , holgura ));
+                    varBasic = new String[tempVB.length+1];
+                    for (int j=0; j<tempVB.length; j++){
+                        varBasic[j] = tempVB[j];
+                    }
+                    varBasic[tempVB.length] = Character.toString(holgura);
+                    tempVB = varBasic;
                     holgura++;
                     break;
                 case 4:
                     restricciones[i].setTipoIgualdad(0);
                     restricciones[i].addMononio( new Monomio( 1 , holgura ));
+                    varBasic = new String[tempVB.length+1];
+                    for (int j=0; j<tempVB.length; j++){
+                        varBasic[j] = tempVB[j];
+                    }
+                    varBasic[tempVB.length] = Character.toString(holgura);
+                    tempVB = varBasic;
                     holgura++;
                     break;
                 default: ;//TODO No hay variables de error para cachar
             }
         }
+    }
+    public String[] getVarBasic(){
+        return varBasic;
+    }
+    public String[] getVar(){
+        return var;
     }
 }
