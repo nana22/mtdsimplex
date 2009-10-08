668621061099191621eae546328114e74fb06d76
marc.antoni.28 <marc.antoni.28@32f8e480-a95e-11de-a118-c7387b1e4f66>
2009-10-08 12:01:57 +0000

    Columnas de la tabla cambiados perfectamente con las funciones

--- Simplex/src/simplex/resolvedor/mate/SimplexTable.java
+++ Simplex/src/simplex/resolvedor/mate/SimplexTable.java
@@ -1,5 +1,6 @@
 package simplex.resolvedor.mate;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author Marco
@@ -8,15 +9,68 @@ public class SimplexTable {
 
 private String colNames[];
 private String rowData[][];
-private String varBasic[];
-private String var[];
-    //FOc = Funcion Objetivo Convertida, restC = Restricciones Convertidas;
-    SimplexTable( String var[], String varBasic[], Ecuacion FOc, Ecuacion restC[] ){
+private String varBasics[];
+private String varNs[];
+private String varTOT[];
+private DefaultTableModel modelo;
+
+    public SimplexTable( String var[], String varBasic[], Ecuacion FOc, Ecuacion restC[] ){
     }
+    
+    public DefaultTableModel SimplexTable( String var[], String varBasic[] ){
+        setVarNs( var );
+        setVarBasics( varBasic );
+        setVarTOT();
+        setColNames ( varNs, varBasics );
+        setRowData();
+        modelo = newTable( colNames );
+        return modelo;
+    }
+
+    public DefaultTableModel newTable( String Colums[] ){
+        JTable jTable1 = new JTable();
+        DefaultTableModel modNuevo = new DefaultTableModel();
+        for (int i=0; i<colNames.length; i++){
+            modNuevo.addColumn(colNames[i]);
+        }
+        return modNuevo;
+    }
+
     public void setColNames( String var[], String varBasic[] ){
-        int tamVar = var.length;
-        int tamVarBasic = varBasic.length;
-        int vars = tamVar + tamVarBasic;
+        int vars = var.length + varBasic.length;
         int numCol = vars + 4;
+        colNames = new String[numCol];
+        colNames[0] = "Variables Basicas";
+        colNames[1] = "Numero Ecuacion";
+        colNames[colNames.length-2] = "b";
+        colNames[colNames.length-1] = "div";
+        for (int i=2, j=0; i<colNames.length-2; i++, j++){
+            colNames[i] = varTOT[j];
+        }
     }
+
+    public void setRowData(){
+        String rowTemp[][] ={ {"1", "2", "3", "4", "5", "6", "7", "8", "9"},
+        {"1", "2", "3", "4", "5", "6", "7", "8", "9"} };
+        rowData = rowTemp;
+    }
+
+    public void setVarBasics( String varBasic[] ){
+        varBasics = varBasic;
+    }
+
+    public void setVarNs( String vars[] ){
+        varNs = vars;
+    }
+
+    public void setVarTOT(){
+        varTOT = new String[varBasics.length + varNs.length];
+        for (int i=0; i<varNs.length; i++){
+            varTOT[i] = varNs[i];
+        }
+        for (int i=0, j=varNs.length; j<varTOT.length; i++, j++ ){
+            varTOT[j] = varBasics[i];
+        }
+    }
+    
 }
