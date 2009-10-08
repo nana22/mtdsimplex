d12cfab250553f62dd9ed11e873965971299f0c9
marc.antoni.28 <marc.antoni.28@32f8e480-a95e-11de-a118-c7387b1e4f66>
2009-10-08 01:12:07 +0000

    Cambios en el main

--- Simplex/src/simplex/Main.java
+++ Simplex/src/simplex/Main.java
@@ -1,33 +1,14 @@
-/*
- * (#)Main.java 1.11
- * @charset "utf-8";
- */
-
 package simplex;
 import javax.swing.UIManager;
 import simplex.ui.VentanaPrincipal;
 
-/**
- *
- * @author Neo Cs || [El Ángel Blanco]
- * @version 1.0
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         try {
-            //new Principal().setVisible(true);
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         }
         catch (Exception ex) {}
         new VentanaPrincipal().setVisible(true);
          
-    }
-
-    static int sumar(int a, int b) {
-        return a + b;
     }
 }
