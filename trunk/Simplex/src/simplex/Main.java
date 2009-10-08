59d4587aa1dd736fb69d0b04f06307438d19489b
marc.antoni.28 <marc.antoni.28@32f8e480-a95e-11de-a118-c7387b1e4f66>
2009-10-08 12:01:57 +0000

    Columnas de la tabla cambiados perfectamente con las funciones

--- trunk/Simplex/src/simplex/Main.java
+++ trunk/Simplex/src/simplex/Main.java
@@ -1,13 +1,22 @@
 package simplex;
 import javax.swing.UIManager;
 import simplex.ui.VentanaPrincipal;
+import simplex.resolvedor.mate.SimplexTable;
 
 public class Main {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         }
-        catch (Exception ex) {}
-        new VentanaPrincipal().setVisible(true);      
+        catch (Exception ex) {}     
+        VentanaPrincipal init = new VentanaPrincipal();
+
+        String variables[] = {"x", "y", "w"};
+        String variablesBasicas[] = {"c", "d", "e", "f"};
+        SimplexTable prueba = new SimplexTable();
+        javax.swing.table.DefaultTableModel model = prueba.SimplexTable(variables, variablesBasicas);
+        init.changeTable(model);
+        init.setVisible(true);
     }
+
 }
