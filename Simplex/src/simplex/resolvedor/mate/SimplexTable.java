d2820c1e344cd9e220ae757090feb14adeb9cd64
marc.antoni.28 <marc.antoni.28@32f8e480-a95e-11de-a118-c7387b1e4f66>
2009-10-08 08:28:54 +0000

    Creando Tabla SimplexTable

---
+++ Simplex/src/simplex/resolvedor/mate/SimplexTable.java
@@ -0,0 +1,19 @@
+package simplex.resolvedor.mate;
+import javax.swing.JTable;
+/**
+ *
+ * @author Marco
+ */
+public class SimplexTable {
+
+private String colNames[];
+private String rowData[][];
+
+    SimplexTable( String var[], String varBasic[], Ecuacion FOc, Ecuacion restC[] ){
+    }
+    public void setColNames( String var[], String varBasic[] ){
+        int tamVar = var.length;
+        int tamVarBasic = varBasic.length;
+        int vars = tamVar + tamVarBasic;
+    }
+}
